89b87626da4d1d2272fd50a4ba191f112d257b0a
parroit <andrea.parodi@ebansoftware.net>
2013-10-14 20:36:19 +0200

    User

---
+++ README.md
@@ -0,0 +1,27 @@
+# ecm-model [![Build Status](https://secure.travis-ci.org/parroit/ecm-model.png?branch=master)](http://travis-ci.org/parroit/ecm-model)
+
+The best project ever.
+
+## Getting Started
+Install the module with: `npm install ecm-model`
+
+```javascript
+var ecm_model = require('ecm-model');
+ecm_model.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 parroit  
+Licensed under the MIT license.
---
+++ test/ecm-model_test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var ecm_model = require('../lib/ecm-model.js');
+
+var expect = require('chai').expect;
+
+var path = require('path');
+
+require('chai').should();
+
+
+describe('messageParser', function () {
+    describe("module", function () {
+        it("should load", function () {
+            expect(ecm_model).not.to.be.equal(null);
+            expect(ecm_model).to.be.a('object');
+
+        });
+    });
+
+    describe("schemas", function () {
+        describe("user", function () {
+            it("is defined",function(){
+                var schema = ecm_model.schemas.user;
+                expect(schema).not.to.be.equal(null);
+                expect(schema).to.be.a('object');
+            });
+        });
+    });
+
+    describe("models", function () {
+        describe("User", function () {
+            it("is defined",function(){
+                var model = ecm_model.models.User;
+                expect(model).not.to.be.equal(null);
+                expect(model).to.be.a('function');
+            });
+        });
+    });
+
+    describe("connect", function () {
+        it("call callback when ready",function(done){
+            ecm_model.connect('mongodb://localhost/ecm',function(schemas,models){
+                expect(schemas).not.to.be.equal(null);
+                expect(schemas).to.be.a('object');
+                expect(models).not.to.be.equal(null);
+                expect(models).to.be.a('object');
+                done();
+            });
+
+        });
+
+    });
+});
